bfdcd8f4eddb842ea1cc4523ad9254ab53e25ef5
Tom Murphy <tmurphy@demonware.net>
2020-04-28 21:22:40 -0700

    Addt

--- cppFiles/src/search.cpp
+++ cppFiles/src/search.cpp
@@ -1,6 +1,3 @@
-//
-// Created by Tom Murphy on 2020-04-21.
-//
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 
@@ -18,21 +15,20 @@
 using namespace std;
 using namespace cv;
 
-//TODO: remove this
+class ImageHashDatabase {
+public:
+    vector<string> imagePaths;
+    vector<tuple<ring_t, string, int>> shapesStrCache;
+    vector<tuple<int, uint64_t, double>> database;
+    HammingWrapper tree;
 
-//TODO: comment
-vector<string> g_images;
-vector<tuple<ring_t, string, int>> g_shapesStrCache;
-
-vector<tuple<int, uint64_t, double>> g_database;
-vector<uint64_t> g_hashes;
-
-
-//FIXME: this shouldn't be a global variable
-HammingWrapper *g_tree;
+    ImageHashDatabase () :
+        tree(64)
+    {};
+};
 
 void addImageToSearchTree(
-        HammingWrapper *tree,
+        ImageHashDatabase &database,
         Mat img_in,
         int thresh,
         int ratio,
@@ -41,9 +37,9 @@ void addImageToSearchTree(
         int areaThresh,
         string imageName)
 {
-    int imageIdx = g_images.size();
+    int imageIdx = database.imagePaths.size();
 
-    g_images.push_back(imageName);
+    database.imagePaths.push_back(imageName);
 
     vector<tuple<ring_t, vector<uint64_t>>> imghashes = getAllTheHashesForImage(
             img_in,
@@ -57,21 +53,87 @@ void addImageToSearchTree(
     for (auto [shape1, shape1hashes] : imghashes) {
         std::stringstream ss;
         ss << bg::wkt(shape1);
-        g_shapesStrCache.push_back( make_tuple(shape1, ss.str(), imageIdx));
+        database.shapesStrCache.push_back(make_tuple(shape1, ss.str(), imageIdx));
         cout << "doing shape : " << ss.str() << " - " << shape1hashes.size() << endl;
         for (int j = 0; j < shape1hashes.size(); j++)
         {
             uint64_t hash1 = shape1hashes[j];
             vector<float> unpacked(64, 0);
-            tree->_unpack(&hash1, &unpacked[0]);
+            database.tree._unpack(&hash1, &unpacked[0]);
 
-            g_database.push_back(make_tuple(g_shapesStrCache.size() - 1, hash1, j));
-            tree->add_item(g_database.size(), &unpacked[0], nullptr);
+            database.database.push_back(make_tuple(database.shapesStrCache.size() - 1, hash1, j));
+            database.tree.add_item(database.database.size(), &unpacked[0], nullptr);
         }
     }
 
-    cout << "Added " << imghashes.size() << " hashes to the search tree." << endl;
+    cout << "Added " << imghashes.size() << " hashes to the search database " << endl;
 }
+
+map<string, map<string, map<string, vector< tuple<uint64_t, uint64_t, int> >>>> findDetailedMatches(
+        ImageHashDatabase &database,
+        Mat img_in2,
+        int thresh,
+        int ratio,
+        int kernel_size,
+        int blur_width,
+        int areaThresh
+        )
+{
+    auto queryImageHashes = getAllTheHashesForImage(
+            img_in2,
+            1,
+            thresh,
+            ratio,
+            kernel_size,
+            blur_width,
+            areaThresh);
+
+    map<string, map<string, map<string, vector< tuple<uint64_t, uint64_t, int> >>>> m;
+    for (auto [queryShape, shapehashes] : queryImageHashes)
+    {
+        std::stringstream ss;
+        ss << bg::wkt(queryShape);
+        cout << "shapehashes.size(): " << shapehashes.size() << " - " << ss.str() << endl;
+        for (auto queryHash : shapehashes) {
+            vector<float> unpacked(64, 0);
+            database.tree._unpack(&queryHash, &unpacked[0]);
+
+            vector<int64_t> result;
+            vector<float> distances;
+            //TODO: is 40 too much here? how does this affect performace? 10 was too little when we have so many rotations
+            database.tree.get_nns_by_vector(&unpacked[0], 40, -1, &result, &distances);
+
+            //Sort the results into our map
+            for (int i = 0; i < result.size(); i++)
+            {
+                if (distances[i] < MATCHING_HASH_DIST)
+                {
+                    auto [shapeIdx, resHash, rotation] = database.database[result[i] - 1];
+                    auto [resShape, resShapeStr, resImgIdx] = database.shapesStrCache[shapeIdx];
+
+                    string imgPath = database.imagePaths[resImgIdx];
+                    if ( m.find(imgPath) == m.end() )
+                        m[imgPath] = map<string, map<string, vector< tuple<uint64_t, uint64_t, int> >>>();
+
+                    if ( m[imgPath].find(ss.str()) == m[imgPath].end() )
+                        m[imgPath][ss.str()] = map<string, vector< tuple<uint64_t, uint64_t, int> >>();
+
+                    cout << "filling with hash: " << resHash << " and rotation: " << rotation << " shape: " << resShapeStr << endl;
+
+                    if ( m[imgPath][ss.str()].find(resShapeStr) == m[imgPath][ss.str()].end() )
+                        m[imgPath][ss.str()][resShapeStr] = vector< tuple<uint64_t, uint64_t, int> >();
+
+                    m[imgPath][ss.str()][resShapeStr].push_back(make_tuple(resHash, queryHash, rotation));
+                }
+            }
+        }
+    }
+
+    return m;
+}
+
+
+ImageHashDatabase *cachedDatabase;
 
 //FIXME: it's weird that img_in is the database image....this is probably a bug
 //FIXME: this is messy and can be cleaned up
@@ -85,15 +147,14 @@ map<string, map<string, vector< tuple<uint64_t, uint64_t, int> >>> findMatchesBe
         int areaThresh,
         bool flushCache
 ) {
-
     if (flushCache) {
         cout << "recomputing cache..." << endl;
-        if (g_tree != nullptr)
-            delete g_tree;
+        if (cachedDatabase != nullptr)
+            delete cachedDatabase;
 
-        g_tree = new HammingWrapper(64);
+        cachedDatabase = new ImageHashDatabase();
         addImageToSearchTree(
-                g_tree,
+                *cachedDatabase,
                 img_in,
                 thresh,
                 ratio,
@@ -102,50 +163,19 @@ map<string, map<string, vector< tuple<uint64_t, uint64_t, int> >>> findMatchesBe
                 areaThresh,
                 "None");
 
-        g_tree->build(20, nullptr);
+        cachedDatabase->tree.build(20, nullptr);
         cout << "...done" << endl;
     }
 
-    auto queryImageHashes = getAllTheHashesForImage(
+    auto resmap = findDetailedMatches(
+            *cachedDatabase,
             img_in2,
-            1,
             thresh,
             ratio,
             kernel_size,
             blur_width,
-            areaThresh);
+            areaThresh
+            );
 
-    map<string, map<string, vector< tuple<uint64_t, uint64_t, int> >>> m;
-    for (auto [queryShape, shapehashes] : queryImageHashes)
-    {
-        std::stringstream ss;
-        ss << bg::wkt(queryShape);
-        cout << "shapehashes.size(): " << shapehashes.size() << " - " << ss.str() << endl;
-        for (auto queryHash : shapehashes) {
-            vector<float> unpacked(64, 0);
-            g_tree->_unpack(&queryHash, &unpacked[0]);
-
-            if ( m.find(ss.str()) == m.end() )
-                m[ss.str()] = map<string, vector< tuple<uint64_t, uint64_t, int> >>();
-
-            vector<int64_t> result;
-            vector<float> distances;
-            //TODO: is 40 too much here? how does this affect performace? 10 was too little when we have so many rotations
-            g_tree->get_nns_by_vector(&unpacked[0], 40, -1, &result, &distances);
-            for (int i = 0; i < result.size(); i++) {
-                if (distances[i] < MATCHING_HASH_DIST) {
-                    auto [shapeIdx, resHash, rotation] = g_database[result[i] - 1];
-                    auto [resShape, resShapeStr, resImgIdx] = g_shapesStrCache[shapeIdx];
-                    cout << "filling with hash: " << resHash << " and rotation: " << rotation << " shape: " << resShapeStr << endl;
-
-                    if ( m.find(ss.str()) == m.end() )
-                        m[ss.str()][resShapeStr] = vector< tuple<uint64_t, uint64_t, int> >();
-
-                    m[ss.str()][resShapeStr].push_back(make_tuple(resHash, queryHash, rotation));
-                }
-            }
-        }
-    }
-
-    return m;
+    return resmap.begin()->second;
 }
